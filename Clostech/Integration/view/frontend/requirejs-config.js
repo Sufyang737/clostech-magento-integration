3214d3d2ff150a58e02657046ccbe00b9550fdf1
LucyAG <lucygauto35@gmail.com>
2026-01-16 20:08:40 -0300

    feat: compile React app with Vite for production-ready Magento integration

---
+++ Clostech/Integration/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    map: {
+        '*': {
+            'Clostech_Integration/js/tryon-bundle': 'Clostech_Integration/js/tryon-bundle'
+        }
+    },
+    shim: {
+        'Clostech_Integration/js/tryon-bundle': {
+            deps: [],
+            exports: 'ClostechtryOn'
+        }
+    }
+};
---
+++ Clostech/Integration/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'view/frontend/web',
+    emptyOutDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/App.jsx'),
+      name: 'ClostechtryOn',
+      formats: ['iife'],
+      fileName: () => 'js/tryon-bundle.js'
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'css/tryon-styles.css'
+          }
+          return assetInfo.name
+        }
+      }
+    }
+  }
+})
