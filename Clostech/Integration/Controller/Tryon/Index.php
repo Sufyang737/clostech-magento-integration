d01484ecaa19124ec06c4adbff92048fa578ec0e
LucyAG <lucygauto35@gmail.com>
2026-01-17 22:12:42 -0300

    feat: add custom controller to serve try-on app via /clostech/tryon route

---
+++ Clostech/Integration/Controller/Tryon/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace Clostech\Integration\Controller\Tryon;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
