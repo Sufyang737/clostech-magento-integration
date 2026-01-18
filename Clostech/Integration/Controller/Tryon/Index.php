2dbc78ceaec1954a5ed2adf1eda3f2959dbc0e47
LucyAG <lucygauto35@gmail.com>
2026-01-17 22:48:17 -0300

    fix: return raw HTML response to avoid RequireJS loading

--- Clostech/Integration/Controller/Tryon/Index.php
+++ Clostech/Integration/Controller/Tryon/Index.php
@@ -3,12 +3,32 @@ namespace Clostech\Integration\Controller\Tryon;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\Result\RawFactory;
+use Magento\Framework\View\LayoutFactory;
 
 class Index extends Action
 {
+    private $resultRawFactory;
+    private $layoutFactory;
+
+    public function __construct(
+        Context $context,
+        RawFactory $resultRawFactory,
+        LayoutFactory $layoutFactory
+    ) {
+        parent::__construct($context);
+        $this->resultRawFactory = $resultRawFactory;
+        $this->layoutFactory = $layoutFactory;
+    }
+
     public function execute()
     {
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $layout = $this->layoutFactory->create();
+        $block = $layout->createBlock('Magento\Framework\View\Element\Template');
+        $block->setTemplate('Clostech_Integration::tryon-full.phtml');
+        
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents($block->toHtml());
+        return $resultRaw;
     }
 }
