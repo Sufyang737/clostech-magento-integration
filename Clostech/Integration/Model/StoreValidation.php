cf0b6aef7755d20085d5c2323391f0fe5208ca0e
LucyAG <lucygauto35@gmail.com>
2026-01-23 17:19:36 -0300

    Update Clostech API URL to production endpoint

--- Clostech/Integration/Model/StoreValidation.php
+++ Clostech/Integration/Model/StoreValidation.php
@@ -276,6 +276,6 @@ class StoreValidation implements StoreValidationInterface
      */
     private function getClostechUrl(): string
     {
-        return 'https://identic-keenan-nonvalorous.ngrok-free.dev';
+        return 'https://portal-empresa.clostech.tech';
     }
 }
